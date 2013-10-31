57722f0a203d0a325973a68f590e38cf4a5d2651
sensey <sensey@vostro.home>
2013-10-31 19:40:55 +0100

    Add new blatts

---
+++ GSA/uebung_2/aufgabe_1.1.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <malloc.h>
+
+long int alignsR (int var1, int var2, int *counter)
+{
+	*counter = *counter +1;
+	if(var1 == 0 || var2 == 0) {
+		return 1;
+	}
+	return alignsR(var1-1, var2, counter) + alignsR(var1, var2-1, counter) + alignsR(var1-1, var2-1, counter);
+}
+
+long int alignsI (int var1, int var2, int *counter)
+{
+	int i, j;
+	int n, m;
+	int x;
+
+	if(var1 > var2) {
+		n = var1+1;
+		m = var2+1;
+	} else {
+		n = var2 + 1;
+		m = var1 + 1;
+	}
+
+	int matrix[15][15];
+
+	for(i = 0; i < m; i++) {
+		for(j = i; j < n; j++) {
+
+			if( i == 0 || j == 0) {
+				matrix[i][j] = 1;
+			} else {
+				*counter = *counter + 3;
+				matrix[i][j] = matrix[i-1][j] + matrix[i][j-1] + matrix[i-1][j-1];
+			}
+
+			if(i!=j && i<=j) {
+				*counter = *counter + 1;
+				matrix[j][i] = matrix[i][j];
+			}
+		}
+	}
+
+	if(var1 > var2) {
+		return matrix[var1][var2];
+	} else {
+		return matrix[var2][var1];
+	}
+}
+
+
+int main (int argc, char *argv[])
+{
+	int counterR = 0, counterI = 0;
+
+	int var1, var2, result;
+
+	if(argc != 3) {
+		printf("Falshe Eingabe!");
+		return(0);
+	}
+
+	var1 = atoi(argv[1]);
+	var2 = atoi(argv[2]);
+
+	printf("n: %d, m: %d Result: %ld ", var1, var2, alignsI(var1, var2, &counterI));
+	printf("(%d accesses)\n", counterI);
+
+	printf("n: %d, m: %d Result: %ld ", var1, var2, alignsR(var1, var2, &counterR));
+	printf("(%d calls)\n", counterR);
+
+	return(0);
+}
