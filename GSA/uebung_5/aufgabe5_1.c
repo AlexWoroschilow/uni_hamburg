9742deae0f4697922b983bced694bb8dd33750e0
Chinney <freddi4ever@hotmail.com>
2013-11-17 20:17:31 +0100

    Update aufgabe5_1.c

--- GSA/uebung_5/aufgabe5_1.c
+++ GSA/uebung_5/aufgabe5_1.c
@@ -197,8 +197,7 @@ int main() {
     alignment alg;
 
 
-    alg.operations = {'R', 'I', 'R', 'D', 'R', 'I', 'R'};
-    alg.index = {7, 2, 2, 1, 3, 1, 3};
+    alg.editop = {}
 
     alg.seq1 = {'a', 'c', 'g', 't', 'a', 'g', 'a', 't', 'a', 't', 'a', 't', 'a', 'g', 'a', 't'};
     alg.seq2 = {'a', 'g', 'a', 'a', 'a', 'g', 'a', 'g', 'g', 't', 'a', 'a', 'g', 'a', 'g', 'g', 'g', 'a'};
