a5b8cb056df79957d15a7e2c1ea55117539df2a6
Felix <felix@ubuntu.(none)>
2013-11-17 15:15:50 +0100

    aufgabe5_1.c

---
+++ GSA/uebung_5/aufgabe5_1.c
@@ -0,0 +1,62 @@
+/* Aufgabe 5.1 GSA Uebungen - Erstellen einer Struktur zum speichern und bearbeiten von Alignments */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+typedef enum {
+  R = 'R', I = 'I', D = 'D'
+} edit;
+  
+typedef enum {
+  a = 'a', c = 'c', g = 'g', t = 't'
+} base;
+
+
+typedef struct {
+
+  int seqlen;
+  int alilen;
+
+  edit operations[];
+  int index[];
+
+  base seq1[seqlen];
+  base seq2[seqlen];
+
+} alignment;
+
+
+int alignment_new(base seq1[], base seq2[]) {
+  
+}
+
+
+int alignment_add_replacement() {
+ 
+}
+
+
+int alignment_add_insertion() {
+ 
+}
+
+
+int alignment_add_deletion() {
+  
+}
+
+int alignment_show() {
+
+}
+
+int alignment_evalcost() {
+  
+}
+
+
+int alignment_delete() {
+  
+}
