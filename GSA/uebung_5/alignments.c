9ef44e23a6239ece97e473ebf8ad7696b2ca3e43
Chinney <freddi4ever@hotmail.com>
2013-11-20 15:05:09 +0100

    Update alignments.c
    header.c

--- GSA/uebung_5/alignments.c
+++ GSA/uebung_5/alignments.c
@@ -233,38 +233,3 @@ int alignment_delete(alignment *alg) {
   
     return(EXIT_SUCCESS);
 }
-
-
-int main() {
-
-    int evalcost;
-
-    char *align = "R7I2R2D1R3I1R3";
-
-    char *seq1 = "acgtagatatatagat";
-    char *seq2 = "agaaagaggtaagaggga";
-    
-    alignment alg;
-    
-    alignment_new(&alg, seq1, seq2, align);
-
-    alignment_show(alg);
-
-    evalcost = alignment_evalcost(alg);
-
-    printf("\tCosts: %d\n\n", evalcost);
-
-    printf("\tadding one deletion...\n");
-    alignment_add_operation(&alg, 1, 'D');
-    alignment_show(alg);
-
-    printf("\tadding one insertion...\n");
-    alignment_add_operation(&alg, 1, 'I');
-    alignment_show(alg);
-
-    printf("\tadding one replacement...\n");
-    alignment_add_operation(&alg, 1, 'R');
-    alignment_show(alg);
-
-    return(EXIT_SUCCESS);
-}
