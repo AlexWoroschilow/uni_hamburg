5cc9e96f82f61743c78f444dead5aa1cd812076c
Chinney <freddi4ever@hotmail.com>
2013-11-21 14:37:58 +0100

    Update alignments.h
    Update DIRK

--- GSA/uebung_5/alignments.h
+++ GSA/uebung_5/alignments.h
@@ -5,33 +5,29 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include "./alignments.c"
 
 
 
-//typedef enum {
-//} edit;
+//typedef enum edit;
 //  
-//typedef enum {
-//} base;
+//typedef enum base;
 
-typedef struct multiedit {
-} multiedit;
-
-typedef struct alignment {
-} alignment;
+typedef struct multiedit multiedit;
 
 
-int alignment_new(alignment *newalg, char seq1[], char seq2[], char align[])
+typedef struct alignment alignment;
 
 
-int alignment_add_operation(alignment *alg, int count, char type)
+alignment alignment_new(char seq1[], char seq2[], char align[]);
 
 
-int alignment_show(alignment alg)
+int alignment_add_operation(alignment *alg, int count, char type);
 
 
-int alignment_evalcost(alignment alg)
+int alignment_show(alignment alg);
 
 
-int alignment_delete(alignment *alg)
+int alignment_evalcost(alignment alg);
+
+
+void alignment_delete(alignment *alg);
