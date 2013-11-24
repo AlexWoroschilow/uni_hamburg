7e43c785a7b3b109a435203e3751583c24dfd36e
christinadebk <christinadebk@gmail.com>
2013-11-25 00:51:40 +0100

    Update editmatrix.h

--- GSA/uebung_6/editmatrix.h
+++ GSA/uebung_6/editmatrix.h
@@ -2,9 +2,9 @@
 #define EDITMATRIX
 
 /*
- * Fill edit matrix by sequenze string
- * return etwas
- */
-int  fillDPtable(char u[], char v[]);
+* Fill edit matrix by sequenze string
+* return etwas
+*/
+int * fillDPtable(char u[], char v[], int E[100][100][4]);
 
 #endif
