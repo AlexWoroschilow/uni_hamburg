b188a059cffb522dab3d5385b5909c9ee766327d
christinadebk <christinadebk@gmail.com>
2013-11-25 00:50:27 +0100

    Update afg61.c

--- GSA/uebung_6/afg61.c
+++ GSA/uebung_6/afg61.c
@@ -6,104 +6,52 @@
 #include "editmatrix.h"
 
 
-void traceback(int matrix[6][6], struct alignment *alg, int i, int j)
+void traceback(int *E[100][100][4], struct alignment *alg)
 {
-    int lenX = alg->seq1len,
-        lenY = alg->seq2len;
-
-    int currentTop = NULL;
-    int currentLeft = NULL;
-    int currentLeftTop = NULL;
-
-    int x, y, xLeft, yLeft, xTop, yTop, xLeftTop, yLeftTop;
+    int i,j;
 
 
-    x = i-1;
-    y = j-1;
+	for (i=100;i>=0;i--) {
+	
+	    for (j=100;j>=0;j--) {
 
-    printf("\n\t%d\t%d\t", x, y);
-
-    while ( x != 0 && y != 0 ) {
-
-        xTop = x;
-        yTop = y-1;
-        xLeft = x-1;
-        yLeft = y;
-        xLeftTop = x-1;
-        yLeftTop = y-1;
-
-        if(i < lenX && j < lenY) {
-
-            if (xTop < lenX && yTop < lenY) {
-                currentTop = matrix[xTop][yTop];
-            }
-            if (xLeft < lenX && yLeft < lenY) {
-                currentLeft = matrix[xLeft][yLeft];
-            }
-            if (xLeftTop < lenX && yLeftTop < lenY) {
-                currentLeftTop = matrix[xLeftTop][yLeftTop];
-            }
-
-            if(currentTop < currentLeft && currentTop < currentLeftTop) {
-                x = xTop;
-                y = yTop;
-                alignment_add_operation(alg, 1, 'D');
-            } else if(currentLeft < currentTop && currentLeft < currentLeftTop) {
-                x = xLeft;
-                y = yLeft;
-                alignment_add_operation(alg, 1, 'I');
-            } else if(currentLeftTop < currentTop && currentLeftTop < currentLeft) {
-                x = xLeftTop;
-                y = yLeftTop;
-                alignment_add_operation(alg, 1, 'R');
-            } else {
-                x = xLeftTop;
-                y = yLeftTop;
-                alignment_add_operation(alg, 1, 'R');
-            }
-        }
-
-        printf("\n\t%d\t%d\t", x, y);
+    		if (E[i][j][3] != 0 ) {
+    			alignment_add_operation(alg, 1, 'R');
+    		} else if (*E[i][j][3] == 0 && *E[i][j][2] != 0) {
+    			alignment_add_operation(alg, 1, 'D');
+    		} else {
+    			alignment_add_operation(alg, 1, 'I');
+    		}	
+    	}
     }
 }
 
 
-int main (int argc, char *argv[])
+int main (int argc, char *argv[]) 
 {
-    int matrix[6][6] = {
-        {1,1,0,0,0,0},
-        {2,2,1,1,0,0},
-        {2,2,3,1,1,0},
-        {1,1,0,0,0,0},
-        {2,2,1,1,0,0},
-        {2,2,3,1,1,0},
-    };
-
-    struct alignment alg;
-
-    /* ich will, dass u und v Strings sind, so dass ich die Längen finden kann */
     char *u = argv[1];
-    char *v = argv[2];
-
-    /* Überprüfen, ob zwei Kommandozeilenparameter eingegeben sind */
-    if(argc != 3 || (sscanf(argv[1], "%s", &*u)) != 1 || (sscanf(argv[2], "%s", &*v)) != 1) {
-        printf("\tBitte zwei Sequenzen angeben\n");
-        exit(EXIT_FAILURE);
+	char *v = argv[2];	
+  	if(argc != 3 || (sscanf(argv[1], "%s", &*u)) != 1 || 
+  						(sscanf(argv[2], "%s", &*v)) != 1) {
+    	printf("\tBitte zwei Sequenzen angeben\n");
+    	exit(EXIT_FAILURE);	
     }
 
 
-    fillDPtable(u, v);
+
+    struct alignment alg;
 
 
-    char seq1[] = "acgtagatatatagat";
-    char seq2[] = "agaaagaggtaagaggga";
-    char seq1_seq2_alg[] = "R7I2R2D1R3I1R3";
+
+	int E[100][100][4];
+    fillDPtable(u,v,E);
+    printf("%d\n",E[3][2][0]);
 
 
-    alg = alignment_new(seq1, seq2, seq1_seq2_alg);
+    alg = alignment_new(u, v, alg);
 
 
-    traceback(matrix, &alg, 4, 4);
+    traceback(E, &alg);
 
 
     alignment_show(alg);
